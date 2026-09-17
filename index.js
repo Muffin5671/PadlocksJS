a2cf270aef44d5331539418fda0d21fc321c4f5f
Muffin5671 <213143890+Muffin5671@users.noreply.github.com>
2026-09-16 20:32:56 -0400

    Create index.js

---
+++ index.js
@@ -0,0 +1,13 @@
+import { Padlock, Time, Score } from "./game/main.js";
+
+const app = new Application();
+
+const options = {
+  width: 480,
+  height: 360,
+  backgroundColor: 0xffffff,
+  antialias: false,
+  preference: "webgl"
+};
+
+await app.init(options);
